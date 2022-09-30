8c1ead60a01b73e32a09eee2071f7bce553f5ae8
Anton <arja.anton@gmail.com>
2022-09-30 11:48:50 +0300

    added hash

--- header.h
+++ header.h
@@ -4,6 +4,8 @@
 
 #include <fstream>
 #include <string>
+#include "MD5.h"
+
 using namespace std;
 string get_fistname();
 string get_lastname();
--- main.cpp
+++ main.cpp
@@ -7,15 +7,19 @@ int main(){
     auto* book = xlCreateXMLBookA();
     if(book){
         auto* sheet = book->addSheet("Sheet1");
-        if(sheet){
+        auto* sheet2 = book->addSheet("Sheet_Hash");
+        if(sheet && sheet2){
             sheet->writeStr(1, 0, "Passenger name");
             sheet->writeStr(1, 1, "Departure");
             sheet->writeStr(1, 2, "Arrival");
             sheet->writeStr(1, 3, "Date");
             sheet->writeStr(1, 4, "Class");
             sheet->writeStr(1, 5, "Ticket cost, $");
+            sheet2->writeStr(1, 0, "Name Hash");
             for(int i = 2; i <= 100001; i++){
-                sheet->writeStr(i, 0, (get_fistname() + " " + get_lastname()).c_str());
+                auto name = get_fistname() + " " + get_lastname();
+                sheet->writeStr(i, 0, name.c_str());
+                sheet2->writeStr(i, 0, md5(name).c_str());
                 auto date = get_date();
                 auto get_airports = get_departure_and_arrival(date.second);
                 sheet->writeStr(i, 1, get_airports.first.c_str());
